432b1080ef8ceefb38e844f527ce142b0c7e4d86
Peter Marton <zsuzsu4588@freemail.hu>
2022-01-26 09:26:15 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+// import React from "react";
+// import { Route, Routes, Outlet, Link } from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="text">
+          <h1 className="animate-charcter">Welcome to Magic Card Finder!</h1>
+          <p>- Search for your favourite Magic cards.</p>
+          <p>- Create your own deck.</p>
+          <p>- Extend your collection.</p>
+          <p>- Create multiple decks.</p>
+        </div>
+
+        <Login user={user} setUser={setUser} />
+        <Register />
+        <Profile user={user} setUser={setUser} />
+      </div>
     </div>
   );
 }
---
+++ src/components/Register.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import React from "react";
+
+function Register() {
+  const [user, setUser] = useState("");
+  const [password, setPassword] = useState("");
+  const baseURL = "http://localhost/user/registeruser";
+
+  const handleUserChange = (e) => setUser(e.target.value);
+
+  const handlePasswordChange = (e) => setPassword(e.target.value);
+
+  const submitForm = async (e) => {
+    e.preventDefault();
+    const payload = JSON.stringify({
+      name: user,
+      password: password,
+    });
+
+    const res = await fetch(baseURL, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: payload,
+    });
+    console.log(await res.json());
+  };
+
+  return (
+    <div>
+      <h1 className="text">Register</h1>
+      <form onSubmit={submitForm}>
+        <label htmlFor="user" className="form">
+          Username:{" "}
+        </label>
+        <input
+          type="text"
+          name="user"
+          value={user}
+          onChange={handleUserChange}
+        ></input>
+
+        <label htmlFor="password" className="form">
+          Password:{" "}
+        </label>
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={handlePasswordChange}
+        ></input>
+        <input type="submit" value="Submit" className="submitbtn"></input>
+      </form>
+    </div>
+  );
+}
+
+// onClick={event =>  window.location.href="./cards.js"}
+
+export default Register;
